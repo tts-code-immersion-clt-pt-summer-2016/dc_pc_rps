ca955578617b3030c64b5289d111622d9ea2aee5
PHILIP CANNING <philip.r.canning@gmail.com>
2016-07-17 16:46:14 -0400

    outputs winner

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,6 +1,8 @@
+#program that lets you play rock paper scissors with the computer
 puts "Let's play Rock Paper Scissors!"
-puts "You go first (throw rock, paper, or scissors)"
+#this section gets the users choice and makes sure user only provides rock, paper, or scissors
 while true
+  puts "Your turn (throw rock, paper, or scissors)"
   user_choice = gets.chomp.downcase
   if (user_choice == "rock"|| user_choice == "paper"|| user_choice == "scissors")
     break
@@ -9,6 +11,26 @@ while true
   end
 end
 
+#this section gets the computers choice
 possible_choices = ['rock', 'paper', 'scissors']
 computer_choice = possible_choices.sample
-puts computer_choice
+
+if (user_choice == "rock" && computer_choice == "rock")
+  puts "You tied! You both threw #{user_choice}."
+elsif (user_choice == "rock" && computer_choice == "paper")
+  puts "Computer wins! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "rock" && computer_choice == "scissors")
+  puts "You win! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "paper" && computer_choice == "paper")
+  puts "You tied! You both threw #{user_choice}."
+elsif (user_choice == "paper" && computer_choice == "rock")
+  puts "You win! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "paper" && computer_choice == "scissors")
+  puts "Computer wins! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "scissors" && computer_choice == "rock")
+  puts "Computer wins! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "scissors" && computer_choice == "paper")
+  puts "You win! You threw #{user_choice} and computer threw #{computer_choice}."
+elsif (user_choice == "scissors" && computer_choice == "scissors")
+  puts "You tied! You both threw #{user_choice}."
+end
