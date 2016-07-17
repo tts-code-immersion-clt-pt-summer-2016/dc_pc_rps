244b528eba140df87ea610bdf32586b9d183fc28
PHILIP CANNING <philip.r.canning@gmail.com>
2016-07-17 16:10:49 -0400

    gets computers choice

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,10 +1,14 @@
 puts "Let's play Rock Paper Scissors!"
 puts "You go first (throw rock, paper, or scissors)"
 while true
-  humans_choice = gets.chomp.downcase
-  if (humans_choice == "rock"|| humans_choice == "paper"|| humans_choice == "scissors")
+  user_choice = gets.chomp.downcase
+  if (user_choice == "rock"|| user_choice == "paper"|| user_choice == "scissors")
     break
   else
     puts "Please only throw rock, paper, or scissors"
   end
 end
+
+possible_choices = ['rock', 'paper', 'scissors']
+computer_choice = possible_choices.sample
+puts computer_choice
