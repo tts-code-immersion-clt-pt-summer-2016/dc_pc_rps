ec39a9617a1b44f7b012278a5d81559653075f32
PHILIP CANNING <philip.r.canning@gmail.com>
2016-07-17 15:59:52 -0400

    gets first choice from human

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,1 +1,10 @@
-puts "let's play rock paper scissors??"
+puts "Let's play Rock Paper Scissors!"
+puts "You go first (throw rock, paper, or scissors)"
+while true
+  humans_choice = gets.chomp.downcase
+  if (humans_choice == "rock"|| humans_choice == "paper"|| humans_choice == "scissors")
+    break
+  else
+    puts "Please only throw rock, paper, or scissors"
+  end
+end
