0a60436e7381c6237622c00965e2ef87e2e4b49d
PHILIP CANNING <philip.r.canning@gmail.com>
2016-07-17 21:21:14 -0400

    last section where it asks if you want to play again

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,4 +1,6 @@
 #program that lets you play rock paper scissors with the computer
+answer = 'yes'
+while answer == 'yes'
 puts "Let's play Rock Paper Scissors!"
 #this section gets the users choice and makes sure user only provides rock, paper, or scissors
 while true
@@ -33,4 +35,16 @@ elsif (user_choice == "scissors" && computer_choice == "paper")
   puts "You win! You threw #{user_choice} and computer threw #{computer_choice}."
 elsif (user_choice == "scissors" && computer_choice == "scissors")
   puts "You tied! You both threw #{user_choice}."
+end
+
+
+puts "Would you like to play again (yes/no)?"
+  while true
+    answer = gets.chomp.downcase
+    if (answer == 'yes' || answer == 'no')
+      break
+    else
+      puts "Please only answer yes or no."
+    end
+  end
 end
